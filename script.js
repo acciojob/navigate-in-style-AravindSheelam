8d429484f80b3704182eaffb444de569af6944eb
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-06-09 23:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function getFormvalue() {
+    //Write your code here
+let firstName=document.getElementById("fname").value;
+let lastName=document.getElementById("lname").value;
+alert(`${firstName} ${lastName}`);
+
+}
